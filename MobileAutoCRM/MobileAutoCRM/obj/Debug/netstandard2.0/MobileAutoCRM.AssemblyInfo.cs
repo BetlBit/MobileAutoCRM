eb964854ee6076d486dc0be029e2ed150f3e5ad7
BetlBit <inthebetmen@mail.ru>
2023-12-18 09:16:14 +0300

    UPD correct AdminPanel

--- MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
+++ MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b55777969a55a448fb8c14c0359281c75831ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9afaeb6de0bad1ae0d7be439414bc63a670955")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
