7e285e1bd723f08264f6c450e947b41711727856
BetlBit <inthebetmen@mail.ru>
2024-01-18 08:41:48 +0300

    Folder allocation

---
+++ MobileAutoCRM/MobileAutoCRM/MainPageAdmin/AdminServices/PageAdminListService.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAutoCRM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAdminListService : ContentPage
+    {
+        public PageAdminListService()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            ShowServices();
+        }
+
+        private void ShowServices()
+        {
+            serviceCollection.ItemsSource = App.Db.GetServices();
+        }
+    }
+}
