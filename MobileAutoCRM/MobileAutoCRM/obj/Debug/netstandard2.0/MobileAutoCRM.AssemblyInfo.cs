559e342b2b0513b32f742483a55cb22acc8636c3
BetlBit <inthebetmen@mail.ru>
2024-01-19 05:55:19 +0300

    UPD correct

--- MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
+++ MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fffa38b2f57d041e471a4901283673973f10754")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
