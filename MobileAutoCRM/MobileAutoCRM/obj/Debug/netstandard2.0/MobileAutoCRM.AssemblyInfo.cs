e4c24d68b23a1868338a6166c93155ca15e5f0f3
BetlBit <inthebetmen@mail.ru>
2024-01-29 02:03:32 +0300

    UPD 7

--- MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
+++ MobileAutoCRM/MobileAutoCRM/obj/Debug/netstandard2.0/MobileAutoCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34207fc4bcd7462f263978e3c3c2412d1631294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileAutoCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
